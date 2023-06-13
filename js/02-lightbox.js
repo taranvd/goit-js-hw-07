f9a72374e51aa322b42a9715f522c8f7e9fddef0
taranvd <apple.vlad.taran@gmail.com>
2023-06-14 02:24:32 +0300

    task 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+const markupGallery = createGalleryMarkup(galleryItems);
+
+galleryList.insertAdjacentHTML("beforeend", markupGallery);
+
+function createGalleryMarkup(items) {
+	return items
+		.map(({ preview, description, original }) => {
+			return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+		})
+		.join("");
+}
+
+var lightbox = new SimpleLightbox(".gallery a", {
+	captionsData: "alt",
+	captionsDelay: 250,
+});
